51db8497f7f74ae2964370b901fe9ff4f569106c
Moursal <moursal888@gmail.com>
2025-01-29 18:05:10 +0100

    ENVOI PARTIE 2 C

---
+++ LesBouclesEnC/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int age=20;
+    if (age>=18){
+
+        printf("Voud etes majeur !\n\n");
+
+    }
+    return 0;
+}
